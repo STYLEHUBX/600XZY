29b366f7134a41fa0c9d809d4e1cf762f7b3661b
vipx600 <sebastiangomez200108@gmail.com>
2024-07-21 19:33:58 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -2,15 +2,18 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $amount = $_POST['amount'];
     $withdrawAddress = $_POST['withdraw-address'];
+    $userEmail = $_POST['email']; // Correo electrónico del usuario
 
-    $to = 'magneginvip@gmail.com';
+    $to = 'magneginvip@gmail.com'; // Correo del destinatario
     $subject = 'Nuevo Retiro de Fondos';
     $message = "Se ha solicitado un retiro de fondos.\n\n";
     $message .= "Monto: $amount\n";
     $message .= "Dirección de retiro: $withdrawAddress\n";
+    $message .= "Correo electrónico del solicitante: $userEmail\n";
 
-    $headers = "From: magneginvip@gmail.com \r\n";
-    $headers .= "Reply-To: no-reply@yourdomain.com\r\n";
+    $headers = "From: no-reply@yourdomain.com\r\n";
+    $headers .= "Reply-To: $userEmail\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     if (mail($to, $subject, $message, $headers)) {
         echo 'Correo enviado con éxito.';
