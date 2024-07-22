d4501eb56550b2c12fa479e142b3fb2a67eacd8a
vipx600 <sebastiangomez200108@gmail.com>
2024-07-22 06:38:15 -0500

    Create guardar_retiro.php

---
+++ guardar_retiro.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $amount = $_POST['amount'];
+    $withdrawAddress = $_POST['withdraw-address'];
+
+    $file = 'RetirosUsuarios.txt';
+    $content = "Monto: $amount, Dirección: $withdrawAddress\n";
+
+    // Escribir en el archivo
+    file_put_contents($file, $content, FILE_APPEND);
+
+    echo "success";
+} else {
+    echo "error";
+}
+?>
